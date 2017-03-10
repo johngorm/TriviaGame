1af008c7ce51edf22222ef1c286af6fe910ca4ce
John Gorman <johngorm@usc.edu>
2017-03-10 15:54:39 -0600

    Display timer and end game if it reaches 0

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,22 +16,27 @@ var numQuestions = quizGame.questions.length;
 
 
 $(document).ready(function() {
-	var seconds = 10;
-	var timerVar;
+    var seconds = 30;
+    var timerVar;
     // Display questions and answers
     var startGame = function() {
         $('#quiz_area').empty();
         var posPointer = 0;
-  
+        // Put seconds counter in top of #quiz_area
+        var $secondCounter = $('<div>');
+        $secondCounter.attr('id','timer');
+        $secondCounter.html('<h1> Time Remaining: ' + seconds + '</h1>')
+        $('#quiz_area').append($secondCounter);
+        //Copy questions to DOM
         quizGame.questions.forEach(function(question) {
             var $question = $('<div>');
-            var $questionPrompt = $('<p>');
+            var $questionPrompt = $('<p class=question-prompt>');
             var $form = $('<form>'); //Why does this have to be outside the second for loop?
 
             $questionPrompt.text(question.prompt);
             $question.append($questionPrompt).addClass('question');
-           	$question.attr('data-position',posPointer);
-
+            $question.attr('data-position', posPointer);
+            //Copy the possible answers and assign a radio button for each
             question.answers.forEach(function(answer) {
                 var $input = $('<input>');
                 var label = answer;
@@ -52,7 +57,7 @@ $(document).ready(function() {
             posPointer++;
 
         });
-        $stopTimeButton = $('<button>');
+        $stopTimeButton = $('<button class="btn btn-lrg btn-warning">');
         $stopTimeButton.text('Stop Time');
         $stopTimeButton.addClass('stop-btn');
 
@@ -63,71 +68,78 @@ $(document).ready(function() {
     };
 
     var startTimer = function() {
-    	 
-   		 timerVar = setInterval(countdown, 1000);
+
+        timerVar = setInterval(countdown, 1000);
     };
 
-    function countdown(){
-    	console.log(seconds--);
-    	if(seconds === -1){
-    		clearInterval(timerVar);
-    	}
-    };	
+    function countdown() {
+        seconds--;
+        $('#timer').empty();
+        $('#timer').html('<h1> Time Remaining: ' + seconds + '</h1>');
+        if (seconds === 0) {
+            endGame();
+        }
+    };
 
     // $('#quiz_area').delegate('[type=radio]', 'click', function() {
-    $('#quiz_area').on( 'click', '[type=radio]', function() {
-    	console.log($(this).val());
+    $('#quiz_area').on('click', '[type=radio]', function() {
+        console.log($(this).val());  //d
     });
 
     $('.start-btn').on('click', function() {
         startGame();
     });
 
-    $('#quiz_area').delegate('.stop-btn','click', function() {
-        clearInterval(timerVar);
-        getResults();
-        
+    $('#quiz_area').delegate('.stop-btn', 'click', function() {
+        // 
+        endGame();
+
     });
 
+    function endGame(){
+        clearInterval(timerVar);
+        getResults();
+    }
 
-    var getResults = function () {
-    
-	
-		var quizForms = $('.question form');
-		
-		for(var ii = 0; ii < quizForms.length; ii++){
-			var questionInput = quizForms[ii].children;
-			var currentQuestion = quizGame.questions[ii];
-			
-			for(var jj = 0; jj < questionInput.length; jj++){
-				if(questionInput[jj].checked && (questionInput[jj].getAttribute('value') === currentQuestion.correctAnswer)){
-					correctGuess++;
-				}
-			}
-		};	
-	
 
-    	displayResults();
+    var getResults = function() {
+
+        var quizForms = $('.question form');
+
+        for (var ii = 0; ii < quizForms.length; ii++) {
+            var questionInput = quizForms[ii].children;
+            var currentQuestion = quizGame.questions[ii];
+
+            for (var jj = 0; jj < questionInput.length; jj++) {
+                var userAnswer = questionInput[jj].getAttribute('value');
+                if (questionInput[jj].checked && (userAnswer === currentQuestion.correctAnswer)) {
+                    correctGuess++;
+                }
+            }
+        };
+
+
+        displayResults();
     };
 
-    var displayResults = function(){
-    	$('#quiz_area').empty();
-    	$('#quiz_area').append($('<p>').html('Results'));
-    	$('#quiz_area').append($('<p>').html('Correct: ' + correctGuess));
-    	$('#quiz_area').append($('<p>').html('Wrong: ' + (numQuestions - correctGuess)));
+    var displayResults = function() {
+        $('#quiz_area').empty();
+        $('#quiz_area').append($('<p>').html('Results'));
+        $('#quiz_area').append($('<p>').html('Correct: ' + correctGuess));
+        $('#quiz_area').append($('<p>').html('Wrong: ' + (numQuestions - correctGuess)));
     };
     //  for(var ii = 0; ii < 4; ii++){
-    // 	var $label = $('<label>');
-    // 	var answerValue = quizGame['questions'][0]['answers'][ii];
-    // 	var $input = $('<input type="checkbox" />');
-    // 	$input.attr('id','checkbox' + ii);
-    // 	$input.attr('value', answerValue);
-    // 	$answers.append($input);
-    // 	console.log($label[0]);
-    // 	//$label.html(answerValue);
-    // 	$label.attr('for','checkbox1');
-    // 	$label.text(answerValue);
-    // 	$answers.append($label);
+    //  var $label = $('<label>');
+    //  var answerValue = quizGame['questions'][0]['answers'][ii];
+    //  var $input = $('<input type="checkbox" />');
+    //  $input.attr('id','checkbox' + ii);
+    //  $input.attr('value', answerValue);
+    //  $answers.append($input);
+    //  console.log($label[0]);
+    //  //$label.html(answerValue);
+    //  $label.attr('for','checkbox1');
+    //  $label.text(answerValue);
+    //  $answers.append($label);
     // }
-    // 	$('#quiz_area').append($answers);
+    //  $('#quiz_area').append($answers);
 });
